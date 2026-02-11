13a705eee9ed13aaf5fd85cb0b6c7efb520e390a
heibais1986 <18032926002@139.com>
2026-02-11 18:05:59 +0800

    update docker-compose.yml

--- panel/vue.config.js
+++ panel/vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
         disableHostCheck: true,
         proxy: {
             '/api': {
-                target: 'https://hou.docman.edu.kg/',
+                target: 'http://127.0.0.1:8080/',
                 //target: 'http://demo2.youdeyunke.com/',
             }
         }
